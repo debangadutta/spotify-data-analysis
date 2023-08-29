6728ef9e4f8ce8636aa95b1a116628926e11b251
Debanga <debangadutta2@gmail.com>
2023-08-29 20:28:20 +0530

    added ETL code to extract album data from Spotify

--- spotify_etl.py
+++ spotify_etl.py
@@ -6,15 +6,32 @@ from datetime import datetime
 import s3fs
 
 client_ID = "c35b9b1e024941e7a1fa9a29815f6e10"
-client_secret = "89db980930da4a47928a14ce645eca8e"
+client_secret = ""
 
 #Spotify Authentication
 auth = SpotifyClientCredentials(client_id=client_ID, client_secret = client_secret)
 
 sp = spotipy.Spotify(client_credentials_manager = auth)
 
-songs = sp.album_tracks(
-    album_id="https://open.spotify.com/album/6PFPjumGRpZnBzqnDci6qJ?si=vRd6OYWuQXKFAObEJU9GMA",
+results = sp.album_tracks(
+    album_id="https://open.spotify.com/album/6PFPjumGRpZnBzqnDci6qJ?si=huhS-psmTUiIkhaS7Eed6g"
     )
 
-print(songs)
+songs = results['items']
+
+while results['next']:
+    results = sp.next(results)
+    songs.extend(results['items'])
+
+song_list = []
+
+for song in songs:
+    refined_song = {
+        'track_number' : song['track_number'],
+        'name' : song['name'],
+        "duration_mins" : round(song['duration_ms']/60000, 2)
+    }
+    song_list.append(refined_song)
+
+df = pd.DataFrame(song_list)
+df.to_csv("linkin_park_album_songs.csv")
